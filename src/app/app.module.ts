d49ac39305d329515eecfbfa6dfc2c9d109fffd2
Li Wenya Colin <wl98336@Colin-MacBookPro.local>
2019-02-13 16:12:12 +0800

    dynamic component loader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,25 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FeatureModule1Module } from './feature-module1/feature-module1.module'
-import {FeatureModule2Module} from './feature-module2/feature-module2.module';
+import { FeatureModule2Module } from './feature-module2/feature-module2.module';
+import {
+  DynamicComponentLoaderModule,
+  DynamicComponentManifest
+} from './dynamic-component-loader/dynamic-component-loader.module';
+
+
+const manifests: DynamicComponentManifest[] = [
+  // {
+  //   componentId: 'message',
+  //   path: 'dynamic-message', // some globally-unique identifier, used internally by the router
+  //   loadChildren: './dynamic-modules/message/message.module#MessageModule'
+  // },
+  // {
+  //   componentId: 'dialog',
+  //   path: 'dialog',
+  //   loadChildren: './dynamic-modules/dialog/dialog.module#DialogModule'
+  // }
+];
 
 @NgModule({
   declarations: [
@@ -14,9 +32,11 @@ import {FeatureModule2Module} from './feature-module2/feature-module2.module';
     BrowserModule,
     AppRoutingModule,
     FeatureModule1Module,
-    FeatureModule2Module
+    FeatureModule2Module,
+    DynamicComponentLoaderModule.forRoot(manifests)
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [AppComponent]
 })
 export class AppModule { }
